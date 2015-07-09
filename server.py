8592854e10d7270bf825cdb971e729d4af51a495
David Robertson <david.m.robertson1@gmail.com>
2015-07-09 20:36:48 +0200

    Connection initation message

--- server.py
+++ server.py
@@ -4,16 +4,29 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 class SCSCProtocol(asyncio.Protocol):
+	#To provide a service, subclass and give this data?
+	service_name = None
+	service_version = None
+	
+	#SCSCP-specific data
+	scscp_versions = "1.3"
+	
 	#Connection callbacks
 	def connection_made(self, transport):
-		...
-		peername = transport.get_extra_info('peername')
-		logging.info('Connection from {}'.format(peername))
+		self.peername = transport.get_extra_info('peername')
+		logging.info('Connection from {}'.format(self.peername))
 		self.transport = transport
+		init_msg = instruction(
+		  'service_name',    self.service_name,
+		  'service_version', self.service_version,
+		  'service_id',      os.getpid(),
+		  'scscp_versions',  self.scscp_versions
+		)
+		transport.write(init_msg.encode('ascii'))
 	
 	def connection_lost(self, exc):
 		logging.info('Lost connection to {}. Exception: {}'.format(
-		  self.transport.get_extra_info('peername'), exc))
+		  self.peername, exc))
 	
 	#Data streaming callbacks
 	def data_received(self, data):
@@ -29,6 +42,18 @@ class SCSCProtocol(asyncio.Protocol):
 	def eof_received(self):
 		...
 
+class DummyService(SCSCProtocol):
+	service_name = "dummy"
+	service_version = "0.0.1"
+
+def instruction(*args):
+	assert len(args) % 2 == 0
+	details = ""
+	pairs = zip(args[::2], args[1::2])
+	for attrib, value in pairs:
+		details += "{}=\"{}\" ".format(attrib, et._escape_attrib(str(value)))
+	return "<?scscp" + details + "?>"
+
 def do_nothing():
 	#Signals like the KeyboardInterrupt are not supported on windows.
 	#This workaround forces the loop to 'check' for keyboard interrupts once a second.
@@ -38,7 +63,7 @@ def do_nothing():
 
 def main():
 	loop = asyncio.get_event_loop()	
-	coro = loop.create_server(SCSCProtocol, '127.0.0.1', 26133)
+	coro = loop.create_server(DummyService, '127.0.0.1', 26133)
 	server = loop.run_until_complete(coro)
 	logging.info('Looking for SCSCP connections on {}'.format(
 	  server.sockets[0].getsockname()))
